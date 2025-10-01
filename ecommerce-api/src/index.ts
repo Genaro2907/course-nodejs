354330cfae9cdae9609d1246a07ee4a26bd12317
Gabriel Genaro <g.genaro.contato@gmail.com>
2025-10-01 15:03:55 -0300

    Post

--- ecommerce-api/src/index.ts
+++ ecommerce-api/src/index.ts
@@ -2,23 +2,34 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get( "/", (req: Request ,res: Response) => {
     res.send("Bem vindo ao curso de nodeJS!")
 });
 
+let usuarios = [{
+    nome: "Gabriel",
+    idade: 24
+},
+{
+    nome: "Genaro",
+    idade: 26
+}]
 
 app.get("/users", (req: Request ,res: Response) => {
-    let usuarios = [{
-        nome: "Gabriel",
-        idade: 24
-    },
-    {
-        nome: "Genaro",
-        idade: 26
-    }]
-
     res.send(usuarios)
 });
+
+app.post("/users", (req: Request, res: Response) => {
+    let user = req.body;
+    usuarios.push(user);
+    res.send({
+        message: "Usuário criado com sucesso!"
+    })
+})
+
+
 
 app.listen(3001, () => {
     console.log("O Servidor está ativo!!");
