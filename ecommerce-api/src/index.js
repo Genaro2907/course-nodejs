1197da689987a9c2fdd779565173cfc1ad71a471
Gabriel Genaro <g.genaro.contato@gmail.com>
2025-10-01 11:05:52 -0300

    Hello World

---
+++ ecommerce-api/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get( "/", (req,res) => {
+    res.send("Bem vindo ao curso de nodeJS")
+})
+
+
+app.listen(3001, () => {
+    console.log("O Servidor está ativo!");
+});
