927904845375975eeca916c6c0ebe0b508419b4b
Gabriel Genaro <g.genaro.contato@gmail.com>
2025-10-01 11:59:25 -0300

    Endpoint Users

--- ecommerce-api/src/index.ts
+++ ecommerce-api/src/index.ts
@@ -1,11 +1,24 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get( "/", (req,res) => {
+app.get( "/", (req: Request ,res: Response) => {
     res.send("Bem vindo ao curso de nodeJS!")
 });
 
+
+app.get("/users", (req: Request ,res: Response) => {
+    let usuarios = [{
+        nome: "Gabriel",
+        idade: 24
+    },
+    {
+        nome: "Genaro",
+        idade: 26
+    }]
+
+    res.send(usuarios)
+})
 
 app.listen(3001, () => {
     console.log("O Servidor está ativo!!");
