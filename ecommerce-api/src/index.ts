3be5a5f76607f39b3f15951dff7c13fb8eebc831
Gabriel Genaro <g.genaro.contato@gmail.com>
2025-10-01 15:28:42 -0300

    Find By Id

--- ecommerce-api/src/index.ts
+++ ecommerce-api/src/index.ts
@@ -8,26 +8,34 @@ app.get( "/", (req: Request ,res: Response) => {
     res.send("Bem vindo ao curso de nodeJS!")
 });
 
-let usuarios = [{
-    nome: "Gabriel",
-    idade: 24
-},
-{
-    nome: "Genaro",
-    idade: 26
-}]
+let id = 0;
+let usuarios: {id: number, name: string, email: string}[] = []
 
 app.get("/users", (req: Request ,res: Response) => {
     res.send(usuarios)
 });
 
+app.get("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let user = usuarios.find(user => user.id === userId);
+
+    res.send(user);
+})
+
 app.post("/users", (req: Request, res: Response) => {
     let user = req.body;
+    user.id = ++id;
     usuarios.push(user);
     res.send({
         message: "Usuário criado com sucesso!"
     })
 })
+
+// app.put("/users/:id", (req: Request, res: Response) => {
+//     let userId = Number(req.params.id);
+//     let userUpdate = req.body;
+//     let user = usuarios.find(user => user.id === userId);
+// })
 
 
 
