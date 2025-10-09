5bb29a5186992b8901cbb8e7c87ced1d30353c9c
Gabriel Genaro <g.genaro.contato@gmail.com>
2025-10-09 11:23:01 -0300

    Validação para JPEG e PNG

--- ecommerce-api/src/services/upload-file.service.ts
+++ ecommerce-api/src/services/upload-file.service.ts
@@ -2,6 +2,7 @@ import fs from "node:fs";
 import { getStorage, getDownloadURL } from "firebase-admin/storage"
 import { fileTypeFromBuffer } from "file-type";
 import { randomUUID } from "node:crypto";
+import { ValidationError } from "../errors/validation.error.js";
 
 export class UploadFileService {
     constructor(private path: string = ""){}
@@ -10,6 +11,14 @@ export class UploadFileService {
         const fileBuffer = Buffer.from(base64, "base64");
 
         const fileType = await fileTypeFromBuffer(fileBuffer);
+
+        if(!fileType) {
+            throw new ValidationError("A extensão do arquivo nao é válida!");
+        }
+
+        if(fileType.mime !== "image/jped" && fileType.mime !== "image/png") {
+            throw new ValidationError("A imagem precisa ser PNG ou JPEG!");
+        }
 
         const fileName = `${randomUUID().toString()}.${fileType?.ext}`;
         
